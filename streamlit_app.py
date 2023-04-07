1ee0160a6a2915bf52862ea9dac118f5030ce68c
adam-intelligen <100817445+adam-intelligen@users.noreply.github.com>
2023-04-07 12:40:52 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,3 +12,18 @@ def init_connection():
     )
 
 conn = init_connection()
+
+# Perform query.
+# Uses st.cache_data to only rerun when the query changes or after 10 min. 
+# (ttl=600)
+@st.cache_data
+def run_query(query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
+
+rows = run_query("SELECT * from source_data_1;")
+
+# Print results.
+for row in rows:
+    st.write(f"{row[0]} has a :{row[1]}:")
